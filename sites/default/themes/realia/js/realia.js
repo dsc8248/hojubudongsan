96be4095ecdb4529eb93692b8b6a83ad5ae7da2b
Taeho Yoo <taeho@unsw.edu.au>
2015-03-24 13:12:56 +1100

    realia.js updated

--- sites/default/themes/realia/js/realia.js
+++ sites/default/themes/realia/js/realia.js
@@ -1,29 +1,56 @@
 Drupal.behaviors.languageSwitch = {
     attach: function (context, settings) {
 
-        var min = parseInt(jQuery('#edit-field-price-value-min').val());
-        var max = parseInt(jQuery('#edit-field-price-value-max').val());
+        if (jQuery('#edit-field-price-value-min').length) {
 
-        jQuery('#edit-field-price-value-wrapper label', context).append('<span class="price"><span class="value"><span class="from">' + min + '</span> - <span class="to">' + max + '</span></span></span>');
-        jQuery('#edit-field-price-value-wrapper .price .value .to', context).currency({ region: settings.theme.currency, thousands: ',', decimal: ',', decimals: 0, prefix: settings.theme.prefix })
-        jQuery('#edit-field-price-value-wrapper .price .value .from', context).currency({ region: settings.theme.currency, thousands: ',', decimal: ',', decimals: 0, prefix: settings.theme.prefix })
+            var min = parseInt(jQuery('#edit-field-price-value-min').val());
+            var max = parseInt(jQuery('#edit-field-price-value-max').val());
 
-        jQuery('#edit-field-price-value-wrapper .views-widget', context).slider({
-            range: true,
-            min: min,
-            max: max,
-            values: [ min, max],
-            slide: function (event, ui) {
-                jQuery('#edit-field-price-value-min').attr('value', ui.values[0]);
-                jQuery('#edit-field-price-value-max').attr('value', ui.values[1]);
+            jQuery('#edit-field-price-value-wrapper label', context).append('<span class="price"><span class="value"><span class="from">' + min + '</span> - <span class="to">' + max + '</span></span></span>');
+            jQuery('#edit-field-price-value-wrapper .price .value .to', context).currency({
+                region: settings.theme.currency,
+                thousands: ',',
+                decimal: ',',
+                decimals: 0,
+                prefix: settings.theme.prefix
+            })
+            jQuery('#edit-field-price-value-wrapper .price .value .from', context).currency({
+                region: settings.theme.currency,
+                thousands: ',',
+                decimal: ',',
+                decimals: 0,
+                prefix: settings.theme.prefix
+            })
 
-                jQuery('#edit-field-price-value-wrapper .price .value .from').text(ui.values[0]);
-                jQuery('#edit-field-price-value-wrapper .price .value .from').currency({ region: settings.theme.currency, thousands: ' ', decimal: ',', decimals: 0, prefix: settings.theme.prefix_currency });
+            jQuery('#edit-field-price-value-wrapper .views-widget', context).slider({
+                range: true,
+                min: min,
+                max: max,
+                values: [min, max],
+                slide: function (event, ui) {
+                    jQuery('#edit-field-price-value-min').attr('value', ui.values[0]);
+                    jQuery('#edit-field-price-value-max').attr('value', ui.values[1]);
 
-                jQuery('#edit-field-price-value-wrapper .price .value .to').text(ui.values[1]);
-                jQuery('#edit-field-price-value-wrapper .price .value .to').currency({ region: settings.theme.currency, thousands: ' ', decimal: ',', decimals: 0, prefix: settings.theme.prefix_currency });
-            }
-        });
+                    jQuery('#edit-field-price-value-wrapper .price .value .from').text(ui.values[0]);
+                    jQuery('#edit-field-price-value-wrapper .price .value .from').currency({
+                        region: settings.theme.currency,
+                        thousands: ' ',
+                        decimal: ',',
+                        decimals: 0,
+                        prefix: settings.theme.prefix_currency
+                    });
+
+                    jQuery('#edit-field-price-value-wrapper .price .value .to').text(ui.values[1]);
+                    jQuery('#edit-field-price-value-wrapper .price .value .to').currency({
+                        region: settings.theme.currency,
+                        thousands: ' ',
+                        decimal: ',',
+                        decimals: 0,
+                        prefix: settings.theme.prefix_currency
+                    });
+                }
+            });
+        }
 
         jQuery('#btn-nav', context).click(function (e) {
             jQuery('body').toggleClass('nav-open');
